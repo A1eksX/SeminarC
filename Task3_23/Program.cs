3d8c61c61e05cb6cc4a5c73b3b6963cba741c445
Alkon <A1kon1.618@yandex.ru>
2022-12-25 13:53:55 +0300

    Д/з по 3 семинару

---
+++ Task3_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число больше 0 ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0) Cube(number);
+else Console.WriteLine ("Введено некорректное число");
+
+void Cube(int numb)
+{
+    for (int i = 1; i <= numb; i++)
+    {
+        Console.WriteLine($"{i,4} | {Math.Pow(i, 3),4}");
+    }    
+}
