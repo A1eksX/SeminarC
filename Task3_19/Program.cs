32ec03ec41c3678ca01866cc51db82ca3de16223
Alkon <A1kon1.618@yandex.ru>
2022-12-25 13:53:55 +0300

    Д/з по семинару 3 №№ 19, 21, 23

--- Task3_19/Program.cs
+++ Task3_19/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+
+if ((number > 9999 && number < 100000) || (number > -100000 && number < -9999))
+{
+    if (number > 0) Console.WriteLine(Palindrome(number) == number ? "Да" : "Нет");
+
+    else if (number < 0)  Console.WriteLine(SubPalindrome(number) == number ? "Да" : "Нет");
+}
+else Console.WriteLine("Вы ввели неверное число.");
+
+
+int Palindrome(int numb)    // введёное число положительное
+
+{
+    for (int i = number; i > 0; i = (i / 10))
+    {
+        int ostatok = i % 10;
+        num = num * 10 + ostatok;
+    }
+    return num;
+}
+
+int SubPalindrome(int numb)     // введёное число отрицательное
+
+{
+    for (int i = number; i < 0; i = (i / 10))
+    {
+        int ostatok = Math.Abs(i) % 10;
+        num = num * 10 + ostatok;
+    }
+    return num * -1;
+}
+
---
+++ Task3_21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки А");
+int xA = Convert.ToInt32(Console.ReadLine());
+int yA = Convert.ToInt32(Console.ReadLine());
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Б");
+int xB = Convert.ToInt32(Console.ReadLine());
+int yB = Convert.ToInt32(Console.ReadLine());
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Range(xA, yA, zA, xB, yB, zB), 2, MidpointRounding.ToZero);   // -   округляем число до 2 знака и MidpointRounding.ToZero не округляем последний знак (показывает как есть)
+
+Console.Write(distance);
+
+double Range (int xA1, int yA1, int zA1, int xB1, int yB1, int zB1)
+{
+    double range = Math.Sqrt(Math.Pow((xA1 - xB1),2) + Math.Pow((yA1-yB1),2) + Math.Pow((zA1-zB1),2));
+    return range;
+}
